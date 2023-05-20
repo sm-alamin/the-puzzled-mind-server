ae864b0a7efbddc2b81745913126b3c987cb9f05
sm-alamin <sma.fkml@gmail.com>
2023-05-20 09:14:34 +0600

    delete toy api is implemented successfully

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ async function run() {
       res.send(result);
     });
 
+    //delete
+    app.delete("/all-toys/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
